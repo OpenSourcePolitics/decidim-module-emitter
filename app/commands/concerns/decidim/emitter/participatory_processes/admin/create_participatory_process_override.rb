0ae2ba5cde8771b6d0c8636c6b216e06e7fe2d2f
stephanie rousset <strabon@club-internet.fr>
2025-01-13 16:10:59 +0100

    feat: update commands

--- app/commands/concerns/decidim/emitter/participatory_processes/admin/create_participatory_process_override.rb
+++ app/commands/concerns/decidim/emitter/participatory_processes/admin/create_participatory_process_override.rb
@@ -10,41 +10,13 @@ module Decidim
           included do
             include ::Decidim::AttachmentAttributesMethods
 
-            def attributes
-              {
-                organization: form.current_organization,
-                title: form.title,
-                subtitle: form.subtitle,
-                weight: form.weight,
-                slug: form.slug,
-                hashtag: form.hashtag,
-                description: form.description,
-                short_description: form.short_description,
-                hero_image: form.hero_image,
-                banner_image: form.banner_image,
-                promoted: form.promoted,
-                scopes_enabled: form.scopes_enabled,
-                scope: form.scope,
-                scope_type_max_depth: form.scope_type_max_depth,
-                private_space: form.private_space,
-                developer_group: form.developer_group,
-                local_area: form.local_area,
-                area: form.area,
-                target: form.target,
-                emitter: form.emitter,
-                emitter_name: form.emitter_name,
-                participatory_scope: form.participatory_scope,
-                participatory_structure: form.participatory_structure,
-                meta_scope: form.meta_scope,
-                start_date: form.start_date,
-                end_date: form.end_date,
-                participatory_process_group: form.participatory_process_group,
-                participatory_process_type: form.participatory_process_type,
-                show_metrics: form.show_metrics,
-                show_statistics: form.show_statistics,
-                announcement: form.announcement
-              }
-            end
+            fetch_file_attributes :hero_image, :emitter
+
+            fetch_form_attributes :organization, :title, :subtitle, :weight, :slug, :hashtag, :description,
+                                  :short_description, :promoted, :scopes_enabled, :scope, :announcement,
+                                  :scope_type_max_depth, :private_space, :developer_group, :local_area, :area, :target,
+                                  :participatory_scope, :participatory_structure, :meta_scope, :start_date, :end_date,
+                                  :participatory_process_group, :participatory_process_type, :emitter_name
           end
         end
       end
--- app/commands/concerns/decidim/emitter/participatory_processes/admin/update_participatory_process_override.rb
+++ app/commands/concerns/decidim/emitter/participatory_processes/admin/update_participatory_process_override.rb
@@ -8,39 +8,13 @@ module Decidim
           extend ActiveSupport::Concern
 
           included do
-            def attributes
-              {
-                title: form.title,
-                subtitle: form.subtitle,
-                weight: form.weight,
-                slug: form.slug,
-                hashtag: form.hashtag,
-                promoted: form.promoted,
-                description: form.description,
-                short_description: form.short_description,
-                scopes_enabled: form.scopes_enabled,
-                scope: form.scope,
-                scope_type_max_depth: form.scope_type_max_depth,
-                private_space: form.private_space,
-                developer_group: form.developer_group,
-                local_area: form.local_area,
-                area: form.area,
-                target: form.target,
-                participatory_scope: form.participatory_scope,
-                participatory_structure: form.participatory_structure,
-                meta_scope: form.meta_scope,
-                start_date: form.start_date,
-                end_date: form.end_date,
-                participatory_process_group: form.participatory_process_group,
-                participatory_process_type: form.participatory_process_type,
-                show_metrics: form.show_metrics,
-                show_statistics: form.show_statistics,
-                announcement: form.announcement,
-                emitter_name: form.emitter_name
-              }.merge(
-                attachment_attributes(:hero_image, :banner_image, :emitter)
-              )
-            end
+            fetch_file_attributes :hero_image, :emitter
+
+            fetch_form_attributes :title, :subtitle, :weight, :slug, :hashtag, :promoted, :description,
+                                  :short_description, :scopes_enabled, :scope, :scope_type_max_depth,
+                                  :private_space, :developer_group, :local_area, :area, :target, :participatory_scope,
+                                  :participatory_structure, :meta_scope, :start_date, :end_date, :participatory_process_group,
+                                  :participatory_process_type, :announcement, :emitter_name
           end
         end
       end
